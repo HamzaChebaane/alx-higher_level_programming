149b99b8fd55c0d6b1f32e99990bf74422a5b285
HamzaChebaane <hamzachbaane@gmail.com>
2023-03-11 16:19:49 -0800

    a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
-#a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+#!/usr/bin/python3
 print('"Programming is like building a multilingual puzzle \n')
